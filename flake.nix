62f24ac7ab339c3f644e938ddec6516385aa3502
Rob Ede <robjtede@icloud.com>
2026-08-02 03:40:22 +0100

    ci: simplify Nix setup

--- flake.nix
+++ flake.nix
@@ -17,6 +17,15 @@
       perSystem = { pkgs, config, inputs', system, lib, ... }: {
         formatter = pkgs.nixpkgs-fmt;
 
+        devShells.ci = pkgs.mkShell {
+          packages = [ pkgs.cargo-nextest pkgs.just ];
+          shellHook = config.x52.justRust.shellHook;
+        };
+
+        devShells.ci-release = pkgs.mkShell {
+          packages = [ inputs'.x52.packages.x52-release-tools ];
+        };
+
         devShells.default = pkgs.mkShell {
           packages = [
             config.formatter
